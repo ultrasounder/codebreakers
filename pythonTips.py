26c0d7bec2d4afeee0fe6b4c3b57d4e776036cb9
ananth vankipuram ananth.vankipuram@gmail.com <vsap78@gmail.com>
2019-11-06 22:42:43 -0800

    adding the source files

---
+++ pythonTips.py
@@ -0,0 +1,10 @@
+#Section one Basic review of the List and its Python built-ins Zip, Enumerate, 
+
+my_list = [] # create an empty list
+print(type(my_list))
+my_list.append(1)
+print(my_list)
+
+for i in range(0,6):
+    my_list.append(i)
+print(my_list)
