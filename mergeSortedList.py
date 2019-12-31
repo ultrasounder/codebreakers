e925cdd658d750991d8ad6b73d82b92acf288317
Andy Younkins <35149320+younkinsa@users.noreply.github.com>
2019-12-30 19:17:11 -0600

    Update mergeSortedList.py

--- mergeSortedList.py
+++ mergeSortedList.py
@@ -13,8 +13,8 @@ head = 1->2->4
 Zipped list: 1 -> 1 -> 2 -> 3 -> 4 -> 4 -> 5 -> 6 -> 7
 # pointers: head, p1, p2, cur
 '''
-
-l3 = ListNode(0) #create a new List node
+## I realize this coding is just for your practice but you should put this in a method signature, even if it isn't a class
+l3 = ListNode(0) #create a new List node ## you can name this node dummy, l3 implies it will be part of the list returned
 curr = l3 # set curr to the new list node
 
 while l1 and l2:
@@ -31,3 +31,4 @@ if l2:
     curr.next = l2
 return l3.next
 
+## Nice work using this dummy node. The code is efficient and the logic is easy to follow. This is very well done. Keep it up!
