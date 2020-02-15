ac396a5b62a03156287599939c8ff7531eb54346
ananth vankipuram ananth.vankipuram@gmail.com <vsap78@gmail.com>
2020-02-15 00:25:37 -0800

    adding minimum sorted array

--- findMinimumSortedArray.py
+++ findMinimumSortedArray.py
@@ -31,9 +31,20 @@ class Solution:
         while left <= right:
             midpoint = left + (right - left) // 2
 
+            if midpoint > 0 and nums[midpoint] < nums[midpoint - 1]:
+                return nums[midpoint]
+            elif nums[left] <= nums[midpoint] and nums[midpoint] > nums[right]:
+                left = midpoint + 1
+            else:
+                right = midpoint - 1
+                
+        
+        
+        
+        
         return nums[left]
 
-        
+
 
         
 
