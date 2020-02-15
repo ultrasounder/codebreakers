e060b1fb31de9d1ecf179f61b26d53b44ff17149
ananth vankipuram ananth.vankipuram@gmail.com <vsap78@gmail.com>
2020-02-14 21:51:49 -0800

    adding files

--- nthFibonacci.py
+++ nthFibonacci.py
@@ -1,11 +1,21 @@
 """APply the formula fib(n-1) + fib(n-2)"""
 
-def nfib(n):
-    if n == 2:
-        return 1
-    elif n == 1:
-        return 0
-    else:
-        return nfib(n-1) + nfib(n-2)
+# def nfib(n):
+#     if n == 2:
+#         return 1
+#     elif n == 1:
+#         return 0
+#     else:
+#         return nfib(n-1) + nfib(n-2)
 
-print(nfib(6))
+# print(nfib(6))
+# #time complexity is 2^n as at each nfib we are calling two operations
+
+# using memoization O(n) time and o(n) space
+def nfib(n, memoize = {1:0, 2:1}):
+    if n in memoize:
+        return memoize[n]
+    else:
+        memoize[n] = nfib(n-1, memoize) + nfib(n -2, memoize)
+        return memoize[n]
+
