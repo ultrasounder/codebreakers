8387fe1ebf5a832652d2ef55b8a8c22f64f2bede
ananth vankipuram ananth.vankipuram@gmail.com <vsap78@gmail.com>
2020-02-15 01:05:46 -0800

    adding searchTwoDArray.py

---
+++ searchTwoDArray.py
@@ -0,0 +1,49 @@
+# Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+# Integers in each row are sorted from left to right.
+# The first integer of each row is greater than the last integer of the previous row.
+# Example 1:
+
+# Input:
+# matrix = [
+#   [1,   3,  5,  7],
+#   [10, 11, 16, 20],
+#   [23, 30, 34, 50]
+# ]
+# target = 3
+# Output: true
+# Example 2:
+
+# Input:
+# matrix = [
+#   [1,   3,  5,  7],
+#   [10, 11, 16, 20],
+#   [23, 30, 34, 50]
+# ]
+# target = 13
+# Output: false
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if len(matrix) == 0:
+            return -1
+        rows = len(matrix)
+        columns = len(matrix[0])
+
+        left = 0
+        right = rows * columns - 1
+
+        while left <= right:
+            midpoint = left  + (right - left) // 2
+
+            midpoint_element = matrix[midpoint // columns][midpoint % columns]
+
+            if midpoint_element == target:
+                return True
+            elif target < midpoint_element:
+                right = midpoint - 1
+            else:
+                left = midpoint + 1
+        return False
+
+
