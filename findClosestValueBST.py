8557962e65da61399dd33bbee4bf13844706f9eb
Andy Younkins <35149320+younkinsa@users.noreply.github.com>
2020-02-18 09:58:07 -0600

    Update findClosestValueBST.py

--- findClosestValueBST.py
+++ findClosestValueBST.py
@@ -10,8 +10,14 @@ def findClosestValueInBstHelper(tree, target, closest):
     if abs(target - closest) > abs(target - tree.value):
         closest = tree.value
     if target < tree.value:
-        return findClosestValueInBstHelper(tree.value, target, closest)
+        return findClosestValueInBstHelper(tree.value, target, closest) # shouldn't this be tree.left?
     elif target > tree.value:
         return findClosestValueInBstHelper(tree.right, target, closest)
     else:
         return closest
+    
+    '''
+    Nice solution. The run time will only be log(N) for a balanced tree. I m assuming you meant this when you wrote 
+    out the runtimes for the proiblem. Nice job using the helper method. We see how effective this is in tackling recursive solutions.
+    Looks like it will return the right answer aside from the small bug I pointed out. Good work!
+    '''
