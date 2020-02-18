b5631b8ec2d2619bf9e2cce2c12adf6cc9c18c02
Andy Younkins <35149320+younkinsa@users.noreply.github.com>
2020-02-18 10:08:39 -0600

    Update maximumWidthBinaryTree.py

--- maximumWidthBinaryTree.py
+++ maximumWidthBinaryTree.py
@@ -13,21 +13,22 @@ class Solution:
         return max_width
 
         
-        
-        
-        
         def _get_widths(root, depth, position):
             if root == None:
                 return 0
-            leftmost_positions.get(depth, self.x)
+            leftmost_positions.get(depth, self.x) # what is this intended to do? Where are we getting self.x from?
             max_width = max(max_width, position - leftmost_positions.get(depth) + 1)
 
             _get_widths(root.left, depth + 1, position * 2)
             _get_widths(root.right, depth +1, position * 2 + 1)
-
-
-
-
+            
+            '''
+            Hi Ananth. I am a bit confused at your approach here. Why do we need depth? It seems like we could just start at position 0
+            and then everytime we move left in our tree we move the position -1 and every time we move right we move the position +1.
+            We keep track of the highest and lowest these go and then we are able to return this. In the case that we need to get the 
+            width of a given level, we can just use our level order traversal approach which gets us a nice level by level print out
+            of the tree. Let me know if you have nay more questions regarding this problem.
+            '''
 
         
 
