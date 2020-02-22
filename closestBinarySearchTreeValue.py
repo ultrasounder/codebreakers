5ea7b9ded6e6b40064913f36f3917da95c0245af
ananth vankipuram ananth.vankipuram@gmail.com <vsap78@gmail.com>
2020-02-22 07:23:35 -0800

    adding the dfs

---
+++ closestBinarySearchTreeValue.py
@@ -0,0 +1,38 @@
+'''270. Closest Binary Search Tree Value
+Easy
+
+569
+
+45
+
+Add to List
+
+Share
+Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+
+Note:
+
+Given target value is a floating point.
+You are guaranteed to have only one unique value in the BST that is closest to the target.
+Example:
+
+Input: root = [4,2,5,1,3], target = 3.714286
+
+    4
+   / \
+  2   5
+ / \
+1   3
+
+Output: 4'''
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def closestValue(self, root: TreeNode, target: float) -> int:
+        
