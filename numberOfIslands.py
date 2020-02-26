30063611a3ece600e66a77d18255c894583f0187
ananth vankipuram ananth.vankipuram@gmail.com <vsap78@gmail.com>
2020-02-26 06:52:15 -0800

    adding numberOfIslands.py

---
+++ numberOfIslands.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfsMark(grid, i, j)
+                    count += 1
+        return count
+        
+        
+        def dfsMark(grid, row, column):
+            if row < 0 or column < 0 or row > len(grid) - 1 or column > len(grid) - 1:
+                return
+            if grid[row][column] == '0':
+                return
+            
+            grid[row][column] = '0' # process a cell
+
+            #call DFS
+            dfsMark(grid, row, column - 1)#DFS left cell
+            dfsMark(grid, row, column + 1)#DFS right cell
+            dfsMark(grid, row - 1, column)#top
+            dfsMark(grid, row + 1, column)#bottom
+
+
+
+
+
+
