14a513336afc9e501fcdf11c40055e9a007bbde1
Andy Younkins <35149320+younkinsa@users.noreply.github.com>
2020-02-27 11:24:45 -0600

    Update numberOfIslands.py

--- numberOfIslands.py
+++ numberOfIslands.py
@@ -10,7 +10,7 @@ class Solution:
         
         
         def dfsMark(grid, row, column):
-            if row < 0 or column < 0 or row > len(grid) - 1 or column > len(grid) - 1:
+            if row < 0 or column < 0 or row > len(grid) - 1 or column > len(grid) - 1: # this check here should be grid[0] because we are checking column, not row
                 return
             if grid[row][column] == '0':
                 return
@@ -22,6 +22,10 @@ class Solution:
             dfsMark(grid, row, column + 1)#DFS right cell
             dfsMark(grid, row - 1, column)#top
             dfsMark(grid, row + 1, column)#bottom
+        '''
+        Nice solution. This is exactly the optimal way of thinking about this problem. Make sure you are careful with this checks
+        because this can actually be a pretty diffcult thing to debug if you are asked to run your coed in an interview. Excellent work!
+        '''
 
 
 
