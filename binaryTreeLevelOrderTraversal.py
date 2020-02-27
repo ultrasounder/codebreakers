e8e4fdea82ad28de2f0afaf3d14273b5096a99d9
Andy Younkins <35149320+younkinsa@users.noreply.github.com>
2020-02-27 11:31:22 -0600

    Update binaryTreeLevelOrderTraversal.py

--- binaryTreeLevelOrderTraversal.py
+++ binaryTreeLevelOrderTraversal.py
@@ -32,21 +32,27 @@ class Solution:
         if root is None:
             return result
 
-        Queue = [self]
+        Queue = [self] # python lists with pop and append are stacks. We should not be using it as a queue. Also, variables should be lowercase. We also want to be passing in root node in
 
-        while Queue is not None:
+        while Queue is not None: # while it isn't empty. We can still have a empty queue but we are still pointing to an object
             size = len(Queue)
 
             currentLevel = []
 
             for i in range(size):
-                current = Queue.pop()
+                current = Queue.pop() # this is the last element. We want to be popping the first element in the list (deque)
                 currentLevel.append(current.val)
                 if current.left is not None:
                     Queue.append(current.left)
                 if current.right is not None:
-                    Queue.append(current.right)
+                    Queue.append(current.right) # nice checks on lines 45-48
         result.append[currentLevel]
+        
+        '''
+        Nice idea with this approach. The queue vs stack implementation is going to give you the wrong answer. If you fix this 
+        then the solution should be correct. Look through the notes and message me if you have any questions about them! You have the
+        right approach which is the hardest part. Just change your implementation a bit and you'll be set!
+        '''
 
 
 
