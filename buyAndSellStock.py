600ba5e6a40e1ab4f5f7e0f95f52a549235dc611
ultrasounder <vsap78@gmail.com>
2020-03-23 15:28:23 -0700

    adding modified buyandsellstock

--- buyAndSellStock.py
+++ buyAndSellStock.py
@@ -1,3 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return None
+        
+
+        minimum_price = prices[0]
+        max_profit = prices[1] - minimum_price
+
+        for current_time in prices:
+            current_price = prices[current_time]
+
+            potential_profit = current_price - minimum_price
+            max_profit = max(potential_profit, max_profit)
+            minimum_price = min(minimum_price, current_price)
+        return max_profit
+
         
