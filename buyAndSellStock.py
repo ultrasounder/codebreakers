bb0b42944939cdc72b297f1370fb7e86c922684e
Andy Younkins <35149320+younkinsa@users.noreply.github.com>
2020-03-31 14:08:40 -0700

    Update buyAndSellStock.py

--- buyAndSellStock.py
+++ buyAndSellStock.py
@@ -6,4 +6,17 @@ class Solution:
 
         for current_time in len(prices) - 1:
             if prices[current_time + 1] > prices[current_time] 
+            
+'''
+This looks like it isn't quite finished yet. I would approach the problem doing something like this:
+
+def maxProfit(prices: List[int]) -> int:
+        smallest = float('inf')
+        curMax = 0
+        for price in prices:
+            smallest = min(smallest, price)
+            curMax = max(curMax, price-smallest)
+        return curMax
         
+Once you get the rest of the code pushed, let me know and I can review it again!
+'''
