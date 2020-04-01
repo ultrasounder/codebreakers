4f387c26cfffc8db32bdf8a198fe32360116d4f0
Andy Younkins <35149320+younkinsa@users.noreply.github.com>
2020-04-01 08:30:41 -0700

    Update minPathSum.py

--- minPathSum.py
+++ minPathSum.py
@@ -24,5 +24,23 @@ class Solution:
         know how this goes!
         '''
         
+        '''
+        Edit 2: the recurrence makes sense, we just want to make sure we are adding to our grid properly. Below I listed a concise
+        way to do so. Basically we look at our dp grid to see the best route so far, and add in the square we are about to explore.
+        By making the dp one bigger than the grid, we can save a lot of code and index out of bounds. Let me know if any of this
+        doesn't make sense and we can discuss!
+        
+        class Solution:
+         def minPathSum(self, grid):
+                m, n = len(grid[0]), len(grid)
+                dp = [[float('inf')] * (m + 1) for i in range(n + 1)]
+                dp[0][1] = dp[1][0] = 0
+
+                for i in range(1, n + 1):
+                    for j in range(1, m + 1):
+                        dp[i][j] = grid[i-1][j-1] + min(dp[i-1][j], dp[i][j-1])
+                return dp[-1][-1]
+        '''
+        
         '''Added the dp array to - AV '''
 
