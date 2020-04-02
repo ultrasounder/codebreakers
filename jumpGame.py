b69c2b43703ef9af2b82c207d6d835efe92e36cd
ananth vankipuram ananth.vankipuram@gmail.com <vsap78@gmail.com>
2020-04-01 23:27:50 -0700

    adding singleNumber

--- jumpGame.py
+++ jumpGame.py
@@ -39,10 +39,10 @@ def houseRobber(arr):
         return nums[0]
     if len(nums) == 2:
         return max(nums[0], nums[1])
-    dp[i] = len(nums)
-    dp[0] = nums[0]
-    dp[1] = max(nums[0], nums[1])
-    for in range(2, len(dp)):
+    dp = [0] * len(nums)#Initalize a DP array for the size of the length of nums array
+    dp[0] = nums[0]# the amount we can get by robbing 2 houses
+    dp[1] = max(nums[0], nums[1]) # for two houses, well max of the first and second house
+    for in range(2, len(dp)): # now for the nth house, our recurrence is current house and two houses before or the previous house
         dp[i] = max(nums[i] + dp[i-2], dp[i-1])
     return dp[n-1]
 
