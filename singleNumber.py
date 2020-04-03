ede00face2c4fc0ce19aead45ac8db3e57817543
Andy Younkins <35149320+younkinsa@users.noreply.github.com>
2020-04-02 22:59:25 -0700

    Update singleNumber.py

--- singleNumber.py
+++ singleNumber.py
@@ -8,3 +8,10 @@ class Solution:
                 seen.add(i)
         for i in seen:
             return i
+'''
+Looks great! This is a great solution and probably the one you would think of in an interview.
+We can also do this in one line: return 2 * sum(set(nums)) - sum(nums)
+The idea here we get the expected sum (on the left) minus the actual (on the right) will give us the number that is not counted twice.
+Same space and time, but more concise and mathy :)
+Looks great!
+'''
